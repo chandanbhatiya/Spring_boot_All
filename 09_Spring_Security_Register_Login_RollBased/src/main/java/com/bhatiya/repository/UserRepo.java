eea979b8711660b23cc4efe60176b0fc84ad3825
chandan kumar <126801728+chandanbhatiya@users.noreply.github.com>
2023-07-29 07:32:06 +0530

    Add files via upload

---
+++ 09_Spring_Security_Register_Login_RollBased/src/main/java/com/bhatiya/repository/UserRepo.java
@@ -0,0 +1,15 @@
+package com.bhatiya.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bhatiya.entity.User;
+import java.util.List;
+
+
+public interface UserRepo  extends JpaRepository<User, Integer>{
+	
+	public User  findByEmail(String email);
+	
+	
+
+}
