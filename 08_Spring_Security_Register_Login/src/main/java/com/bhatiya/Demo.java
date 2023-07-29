045da7027f1695c41a5d70ba859908c43463b968
chandan kumar <126801728+chandanbhatiya@users.noreply.github.com>
2023-07-29 07:30:33 +0530

    Add files via upload

---
+++ 08_Spring_Security_Register_Login/src/main/java/com/bhatiya/Demo.java
@@ -0,0 +1,11 @@
+package com.bhatiya;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Demo {
+		public static void main(String[] args) {
+			System.out.println(new BCryptPasswordEncoder().encode("1234"));
+		}
+	}
+
+
---
+++ 08_Spring_Security_Register_Login/src/main/java/com/bhatiya/service/UserService.java
@@ -0,0 +1,11 @@
+package com.bhatiya.service;
+
+import com.bhatiya.entity.User;
+
+public interface UserService {
+	
+public User saveUser(User user);
+
+public void removeSessionMessage();
+
+}
